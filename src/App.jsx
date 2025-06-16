4fb41536b03951337a4be98ee3b77f2e329d3896
Shubham Kumar <shubhamkumarchoudharyy@gmail.com>
2025-06-16 20:08:43 +0530

    Add SelectInput component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import "./App.css";
+import SelectInput from "./components/ui/SelectInput";
 
 function App() {
   return (
     <>
-      <div className="App">CRM</div>
+      <SelectInput
+        label="Timezone"
+        name="timezone"
+        value={""}
+        onChange={() => {}}
+        options={[
+          { label: 'UTC', value: 'UTC' },
+          { label: 'IST', value: 'Asia/Kolkata' }
+        ]}
+      />
     </>
   );
 }
 
 export default App;
+
+
