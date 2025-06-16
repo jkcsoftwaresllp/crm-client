2829bf31c51ebcd7ec829bf0d891fb2ea6984f10
Nitin-777 <nitinsharmaop828@gmail.com>
2025-06-16 15:25:57 +0530

    Email Input Component (#8)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
-
+import EmailInput from "./components/ui/EmailInput";
 function App() {
   return (
     <>
-      <div className="App">CRM</div>
+    <EmailInput />
+      
     </>
   );
 }
