cb096ff1e8f466213cb1e2e7da4317bda2096eea
Khushboo vaswal <khushboovaswal321@gmail.com>
2025-06-17 13:41:41 +0530

    Updated Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,12 @@
 import "./App.css";
-import { BasicInputs } from "./components/testFolder/BasicInputs";
-import SelectInput from "./components/ui/SelectInput";
+import { MainTesting } from "./components/testFolder/MainTesting";
 
 function App() {
 
   
   return (
     <>
-    <BasicInputs/>
-      <SelectInput
-        label="Timezone"
-        name="timezone"
-        value={""}
-        onChange={() => {}}
-        options={[
-          { label: 'UTC', value: 'UTC' },
-          { label: 'IST', value: 'Asia/Kolkata' }
-        ]}
-      />
+    <MainTesting/>
     </>
   );
 }
