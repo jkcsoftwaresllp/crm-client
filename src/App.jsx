d3eb7fdd7e0cdb5726d29fc56881a8d40e61f9ae
Avinash kr. <jhaavinash110044@gmail.com>
2025-06-17 09:34:02 +0530

    feat: add Textinput (#9)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import "./App.css";
 import EmailInput from "./components/ui/EmailInput";
+import TextInput from "./components/ui/TextInput";
+
+
 function App() {
   return (
     <>
     <EmailInput />
-      
+    <TextInput />  
     </>
   );
 }
 
 export default App;
+
+
+
+       
+
+
